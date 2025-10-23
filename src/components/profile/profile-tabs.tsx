24cf3f23a85129c62c0b56ddea5d909f93501380
Ayush singh <ayushkrsingh204@gmail.com>
2025-10-23 15:58:14 +0000

    Display shadow bogger

--- src/components/profile/profile-tabs.tsx
+++ src/components/profile/profile-tabs.tsx
@@ -11,7 +11,7 @@ export function ProfileTabs() {
   return (
     <div className="p-4">
       <Tabs defaultValue="about" className="w-full">
-        <TabsList className="grid w-full grid-cols-3 bg-neutral-900 p-1 rounded-full shadow-[0_4px_12px_rgba(0,0,0,0.4),_inset_0_2px_4px_rgba(255,255,255,0.05)]">
+        <TabsList className="grid w-full grid-cols-3 bg-neutral-900 p-1 rounded-full shadow-[0_8px_24px_rgba(0,0,0,0.5),_inset_0_2px_4px_rgba(255,255,255,0.05)]">
           <TabsTrigger value="about" className="rounded-full text-gray-400 data-[state=active]:bg-neutral-800 data-[state=active]:text-white data-[state=active]:shadow-[inset_0_2px_4px_rgba(0,0,0,0.6),_0_1px_1px_rgba(255,255,255,0.05)]">
             <User className="mr-2 h-4 w-4" /> About Me
           </TabsTrigger>
